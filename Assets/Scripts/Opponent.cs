e51dd320ea91ce633caa41da603915f479d488de
Ahmet AVCI <ahmt_ua@hotmail.com.tr>
2023-06-21 21:25:33 +0300

    Added SpeedBoost Updates
    Added SpeedBoost Updates

--- Assets/Scripts/Opponent.cs
+++ Assets/Scripts/Opponent.cs
@@ -31,4 +31,21 @@ public class Opponent : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Speedboost"))
+        {
+            OpponentAgent.speed = OpponentAgent.speed + 3f;
+            speedBoosterIcon.SetActive(true);
+            StartCoroutine(SlowAfterAWhileCoroutine());
+        }
+    }
+
+
+    private IEnumerator SlowAfterAWhileCoroutine()
+    {
+        yield return new WaitForSeconds(2.0f);
+        OpponentAgent.speed = OpponentAgent.speed - 3f;
+        speedBoosterIcon.SetActive(false);
+    }
 }
